f511592234ff129538977ec4540500ea2a304ff4
eacarras <enriqueaaron7@gmail.com>
2020-04-10 08:38:01 -0500

    Add verification of strength to the password

--- password_strength/handler.js
+++ password_strength/handler.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const verifyLength = require('./constraints/verify_password_length');
+const verifyStrength = require('./constraints/verify_password_strength');
 
 
 module.exports.password = async event => {
   try {
     const { password } = event.pathParameters;
+
     await verifyLength(password);
+    const { score } = await verifyStrength(password);
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'You password is strong!.'
+        message: `You password is strong!. Score: ${score}.`
       })
     }
   } catch (e) {
@@ -19,7 +22,8 @@ module.exports.password = async event => {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Error! ' + e.message + '.'
+        message: 'Error! ' + e.message + '.',
+        score: e.score ? `Score: ${e.score}` : 'Improve the length of your pass to see this.'
       })
     }
   } 

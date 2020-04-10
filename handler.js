4fd0b17f04fea2812ad0e39485e67349c3e3bf88
eacarras <enriqueaaron7@gmail.com>
2020-04-10 07:31:28 -0500

    Create example of POST method

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
 'use strict';
+const queryString = require('querystring')
+
 
 module.exports.hello = async event => {
   return {
@@ -6,6 +8,22 @@ module.exports.hello = async event => {
     body: JSON.stringify(
       {
         message: `Hello ${event.pathParameters.name}`,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.showUser = async event => {
+  const body = queryString.parse(event['body']);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Simple example of POST reequest`,
+        input: `Hello ${body.name}`
       },
       null,
       2
